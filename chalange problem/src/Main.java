60686f877b339caf47fb29c5f3614d98e82fc511
HG-55 <77021681+HG-55@users.noreply.github.com>
2021-02-04 23:20:55 -0600

    Add files via upload

---
+++ chalange problem/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+	
+	/*
+	 Create Method where you input a string that has numbers in it
+	 
+	 "hello 90, where is 2"
+	 
+	 Return a string where you add n to every int in the strin
+	 
+	 n = 9, -> "hello 99, where is 11"
+	  
+	 * */
+	public static void addNine(String n){
+		string
+	}
+	
+	/*
+	 Create Method where you enter a string. Turn all the numbers into letters
+	 
+	 h3ll0 -> hthreellzero
+	 
+	 * */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
